b91ba2e2310a6960c76d6d4d65a24c1fae4a3eb4
Svenw <74722783+svenwlive@users.noreply.github.com>
2024-09-06 06:17:05 +0000

    Overwrite all files in 'parent' with files in 'snake-online-updating'

--- snake-online-updating/load-script.js
+++
@@ -1,3 +0,0 @@
-console.log('Hello!');
-console.log('This website was created in GitHub with GitHub Codespaces!');
-console.log('Created by svenwlive over at https://github.com/svenwlive')
--- snake-online-updating/start-server.sh
+++
@@ -1,1 +0,0 @@
-http-server -d -c-1 -t10 --no-dotfiles --log-ip -e html
